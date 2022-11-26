f4717927dff0f0b1aec02fd63dcb36d79030e2bd
dåshuång li <li.dashuang@icloud.com>
2022-11-27 00:34:11 +0800

    Update run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,2 @@
 
-docker rm fhc
-docker run -d -p 8081:8081 -p 8080:8080 --name=fhc --restart=always fake-healthcode:latest
+docker run -d -p 8081:8081 -p 8080:8080 --name=fhc --restart=always -v healthcode.py:/scripts/healthcode.py fake-healthcode:latest
