07e8ded6828e3fe925bdaf4c130da88ab993be1f
dåshuång li <li.dashuang@icloud.com>
2022-12-01 02:35:56 +0800

    Update hefei.js

--- hefei.js
+++ hefei.js
@@ -1,3 +1,8 @@
+
+// [Script]
+// 安康码 = type=http-response,pattern=https://akm.ahzwfw.gov.cn/akm-sj-user/health/detail,requires-body=1,max-size=1048576,debug=1,script-path=https://rules-set.oss-cn-beijing.aliyuncs.com/hefei.js,script-update-interval=300
+
+
 let obj = JSON.parse($response.body);
 
 obj.data.healthStatus = "0";
