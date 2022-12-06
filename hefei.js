f33776b851786e8b0ba16375525e3ee1300bfdac
dåshuång li <li.dashuang@icloud.com>
2022-12-06 14:33:30 +0800

    Update hefei.js

--- hefei.js
+++ hefei.js
@@ -1,6 +1,6 @@
 
 // [Script]
-// 安康码 = type=http-response,pattern=https://akm.ahzwfw.gov.cn/akm-sj-user/health/detail,requires-body=1,max-size=1048576,debug=1,script-path=https://rules-set.oss-cn-beijing.aliyuncs.com/hefei.js,script-update-interval=300
+// hefei= type=http-response,pattern=https://akm.ahzwfw.gov.cn/akm-sj-user/health/detail,requires-body=1,max-size=1048576,debug=1,script-path=https://rules-set.oss-cn-beijing.aliyuncs.com/hefei.js,script-update-interval=300
 
 // deploy 
 //ossutilmac64 cp -f hefei.js  oss://rules-set/hefei.js
